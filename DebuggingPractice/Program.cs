6c80e52cef7aabe81d1510ac0b1bc37ab99b44ea
sbr-vincent <vincent.g.sbr@gmail.com>
2023-09-27 14:14:55 -0700

    Completed a Core and Practice Assignment

---
+++ DebuggingPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿// Challenge 1
+bool amProgrammer = true;
+int Age = 30;
+List<string> Names = new List<string>();
+Names.Add("Monica");
+Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+MyDictionary.Add("Hello", "0");
+MyDictionary.Add("Hi there", "0");
+// This is a tricky one! Hint: look up what a char is in C#
+string MyName = "MyName";
+// Challenge 2
+List<int> Numbers = new List<int>() {2,3,6,7,1,5};
+for(int i = Numbers.Count-1; i >= 0; i--)
+{
+    Console.WriteLine(Numbers[i]);
+}
+// Challenge 3
+List<int> MoreNumbers = new List<int>() {12,7,10,-3,9};
+foreach(int i in MoreNumbers)
+{
+    Console.WriteLine(i);
+}
+// Challenge 4
+// I would change this to a regular for loop so that I could modify the value at the index
+List<int> EvenMoreNumbers = new List<int>() {3,6,9,12,14};
+foreach(int num in EvenMoreNumbers)
+{
+    if(num % 3 == 0)
+    {
+        num = 0;
+    }
+}
+
+// Challenge 5
+// What can we learn from this error message?
+// Maybe that the string is immutable? After checking the internet it looks like they are
+string MyString = "superduberawesome";
+MyString[7] = "p";
+
+// Challenge 6
+// Hint: some bugs don't come with error messages
+// It would never reach 12 because the number is exclusive.
+Random rand = new Random();
+int randomNum = rand.Next(13);
+Console.WriteLine(randomNum);
+if(randomNum == 12)
+{
+    Console.WriteLine("Hello");
+}
+
+
---
+++ FundamentalsII/Program.cs
@@ -0,0 +1,40 @@
+﻿// Three Basic Arrays
+int[] arr = {0,1,2,3,4,5,6,7,8,9};
+string[] names = {"Tim", "Martin", "Nikki", "Sara"};
+Boolean[] arr2 = new Boolean[10];
+
+for (int i = 0; i < arr2.Length; i += 2){
+    arr2[i] = true;
+}
+
+foreach(Boolean idx in arr2){
+    Console.WriteLine(idx);
+}
+
+// List of Flavors
+List<string> iceCreams = new();
+iceCreams.Add("Vanilla");
+iceCreams.Add("Strawberry");
+iceCreams.Add("Chocolate");
+iceCreams.Add("Peanut Butter");
+iceCreams.Add("Salted Caramel");
+Console.WriteLine(iceCreams.Count);
+Console.WriteLine(iceCreams[2]);
+iceCreams.RemoveAt(2);
+Console.WriteLine(iceCreams.Count);
+
+// User Dictionary
+Dictionary<string, string> User = new();
+User.Add(names[0], iceCreams[0]);
+User.Add(names[1], iceCreams[1]);
+User.Add(names[2], iceCreams[2]);
+User.Add(names[3], iceCreams[3]);
+
+
+foreach(KeyValuePair<string,string> item in User)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
+
+
+

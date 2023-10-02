1d50ec32d0c6b21b1e2d7bd57d1128d771c45376
sbr-vincent <vincent.g.sbr@gmail.com>
2023-10-02 13:50:50 -0700

    Completed Ride Maker Practice

---
+++ RideMaker/Program.cs
@@ -0,0 +1,13 @@
+﻿Vehicle v = new Vehicle("Hyundai", 5, "silver", true);
+Vehicle v2 = new Vehicle("Corvette", 2, "grey green", true);
+Vehicle v3 = new Vehicle("Toyota", "blue");
+Vehicle v4 = new Vehicle("Hyundai", "white");
+
+List<Vehicle> lot = new List<Vehicle>() {v, v2, v3, v4};
+foreach(Vehicle car in lot){
+    car.ShowInfo();
+}
+
+v.Travel(100);
+v.ShowInfo();
+
---
+++ RideMaker/Vehicle.cs
@@ -0,0 +1,35 @@
+class Vehicle{
+    public string Name;
+    public int NumOfPassengers;
+    public string CarColor;
+    public bool HasEngine;
+    private double Miles = 0;
+
+    public Vehicle(string n, int passengers, string c, bool engine){
+        Name = n;
+        NumOfPassengers = passengers;
+        CarColor = c;
+        HasEngine = engine;
+    }
+
+    public Vehicle(string n, string color){
+        Name = n;
+        CarColor = color;
+        NumOfPassengers = 4;
+        HasEngine = true;
+    }
+
+    public void ShowInfo(){
+        Console.WriteLine($"Name of the vehicle: {Name}");
+        Console.WriteLine($"Number of Passengers: {NumOfPassengers}");
+        Console.WriteLine($"Car color: {CarColor}");
+        Console.WriteLine($"Does it have an engine: {HasEngine}");
+        Console.WriteLine($"Number of miles driven: {Miles}\n");
+    }
+
+    public string Travel(int distance){
+        Miles += distance;
+
+        return $"Your vehicle had gone {Miles} miles.";
+    }
+}
